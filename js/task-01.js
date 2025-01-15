d4a4096fafba914cd3c9d40ba0a6aa1dd348a736
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-15 22:20:16 +0200

    add goit-js-hw-07

--- js/task-01.js
+++
@@ -1,6 +0,0 @@
-const categoriesWithItems = document.querySelectorAll('li.item')
-categoriesWithItems.forEach(element => {
-    console.log(`Category: ${element.firstElementChild.textContent}`);
-    console.log(`Elements: ${element.lastElementChild.children.length}`);
-});
-
